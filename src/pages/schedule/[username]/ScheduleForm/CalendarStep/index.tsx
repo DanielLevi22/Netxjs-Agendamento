ce80fe660a941420bd3c8366421f15c04ddd6348
Daniel <daniellevi000@gmail.com>
2023-10-02 12:01:55 -0300

    add table schedules

--- src/pages/schedule/[username]/ScheduleForm/CalendarStep/index.tsx
+++ src/pages/schedule/[username]/ScheduleForm/CalendarStep/index.tsx
@@ -6,17 +6,26 @@ import {
   TimerPickerItem,
   TimerPickerList,
 } from './styles'
+import { useState } from 'react'
+import dayjs from 'dayjs'
 
 export function CalendarStep() {
-  const IsDateSelected = false
+  const [selectedDate, setSelectedDate] = useState<Date | null>(null)
+
+  const IsDateSelected = !!selectedDate
+
+  const weekDay = selectedDate ? dayjs(selectedDate).format('dddd') : null
+  const describeDate = selectedDate
+    ? dayjs(selectedDate).format('DD[ de ]MMMM')
+    : null
 
   return (
     <Container isTimePickerOpen={IsDateSelected}>
-      <Calendar />
+      <Calendar selectedDate={selectedDate} onDateSelected={setSelectedDate} />
       {IsDateSelected && (
         <TimerPicker>
           <TimerHeader>
-            Terca-feira <span>20 de setembro</span>
+            {weekDay} <span>{describeDate}</span>
           </TimerHeader>
 
           <TimerPickerList>
